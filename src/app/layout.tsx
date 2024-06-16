78e7d673176da87c2d3bdaba56f3ad42ff6b3d49
FilipeHibrael <filipehibrael27@gmail.com>
2024-06-16 18:35:00 -0300

    font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import { typeInter } from '@/functions/fonts';
 
 export const metadata: Metadata = {
   title: 'Clock App',
@@ -12,7 +13,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body style={{ fontFamily: `${typeInter.style.fontFamily}` }}>
+        {children}
+      </body>
     </html>
   );
 }
---
+++ src/functions/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter } from 'next/font/google';
+
+export const typeInter = Inter({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+  variable: '--type-inter',
+});
