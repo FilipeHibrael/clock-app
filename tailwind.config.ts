cacb824f046dd241e5430f172072c17dfdfc8ff0
FilipeHibrael <filipehibrael27@gmail.com>
2024-06-16 20:24:24 -0300

    colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,16 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  theme: {
+    extend: {
+      colors: {
+        'color-black': '#0F0F0F',
+        'color-dark-grey': '#303030',
+        'color-ligh-grey': '#999999',
+        'color-white': '#F5F5F5',
+      },
+    },
+  },
   plugins: [],
 };
 export default config;
